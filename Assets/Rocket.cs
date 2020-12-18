f7c32128011d4cba89863aa7462b4b6f20d75401
Bryan <windsor.bryan@gmail.com>
2020-12-18 19:59:07 +0100

    Added audio source

---
+++ Assets/Rocket.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    Rigidbody rigidbody;
+    AudioSource m_MyAudioSource;
+
+    //Play the music
+    bool m_Play;
+    //Detect when you use the toggle, ensures music isn’t played multiple times
+    bool m_ToggleChange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        m_MyAudioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if(!m_MyAudioSource.isPlaying)
+            {
+                m_MyAudioSource.Play();
+            }
+            rigidbody.AddRelativeForce(Vector3.up);
+        }
+        else
+        {
+            m_MyAudioSource.Stop();
+        }
+        if(Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward);
+        }
+
+    }
+}
